e301b751a551cefe4cfbf5d3e53f54cea098ec99
Benson Sanga <bensonsanga@hotmail.com>
2019-04-04 22:35:14 +0200

    change of stat

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+require('dotenv').config()
+
+app.set('port', (process.env.PORT || 5000));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
+
+app.get('/', function(request, response) {
+  var env = process.env.APP_ENV;
+  if (env == 'staging') {
+    var envName = 'staging'
+  } else if (env == 'production') {
+    var envName = 'production'
+  } else {
+    var envName = 'review app'
+  }
+  response.render('index.html', { env: envName});
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app running at localhost:" + app.get('port'));
+});
+
+module.exports = app
